174e909b8d8ec14bf23e48ea329d23e0fd78b0e5
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-23 10:40:44 +0545

    Add Enrolments model class

---
+++ WebApplication1/Models/Enrolments.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Enrolments
+{
+    public int EnrolmentID { get; set; }
+    public int StudentID { get; set; }
+    public string EnrolCourseID { get; set; }
+    public DateTime EnrolDate { get; set; }
+}
