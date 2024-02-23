da8d78239a74e8d149bcb0aad2838d49ec4f2b1d
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-23 10:40:50 +0545

    Add Instructors model class

---
+++ WebApplication1/Models/Instructors.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models;
+
+public class Instructors
+{
+    public int InstructorID { get; set; }
+    public string CourseInstructorName { get; set; }
+    public string Contact { get; set; }
+    public DateTime DOB { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Country { get; set; }
+}
