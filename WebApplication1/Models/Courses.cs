70f1b02a4252e2d056278ee5c6278d0572ab22e5
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-23 10:40:22 +0545

    Add Courses model class

---
+++ WebApplication1/Models/Courses.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models;
+
+public class Courses
+{
+    public string CourseID { get; set; }
+    public string CourseTitle { get; set; }
+    public int CourseInstructorID { get; set; }
+}
