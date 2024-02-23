6964b03ac26c68e7f22fdcfbdb3510644d73607b
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-23 10:41:02 +0545

    Add Students model class

---
+++ WebApplication1/Models/Students.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models;
+
+public class Students
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public string Contact { get; set; }
+    public DateTime DOB { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Country { get; set; }
+}
