18444f1676fd3c95115ba889c6398483be0d9946
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-23 10:40:56 +0545

    Add Lessons model class

---
+++ WebApplication1/Models/Lessons.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models;
+
+public class Lessons
+{
+    public int LessonID { get; set; }
+    public int LessonNo { get; set; }
+    public string LessonTitle { get; set; }
+    public string LessonStatus { get; set; }
+    public DateTime LastAccessedDate { get; set; }
+    public string CourseID { get; set; }
+
+}
